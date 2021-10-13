257f1cbd3d44868b06ebf3e291062f4d3a24714c
Savannah Leasman <leasman7@gmail.com>
2021-10-13 11:22:54 -0500

    created files for actual bad-bank

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-function Spa() {
-    const Route = ReactRouterDOM.Route;
-    const Link = ReactRouterDOM.Link;
-    const MashRouter = ReactRouterDOM.MashRouter;
-
+function Spa(){
     return (
-        <HashRouter>
-            <div>
-                <h1>Routing - Hellow World</h1>
-                <Link to="/">Home</Link>
-                <Link to="/about/">About</Link>
-                <Link to="/products/">Products</Link>
-                <hr/>
-                <Route path="/" exact component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/products" component={Products} />
-            </div>
-        </HashRouter>
+        <>
+        <h1>Welcome to Bad Bank</h1>
+        <NavBar/>
+        </>
     );
 }
+
+ReactDOM.render(
+    <Spa/>,
+    document.getElementById('root')
+)
