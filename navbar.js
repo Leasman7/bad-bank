746ad93cd8d06d393fffc31a03c6cf3aaaf470c4
Savannah Leasman <leasman7@gmail.com>
2021-10-13 12:53:47 -0500

    styled navbar with bootstrap

--- navbar.js
+++ navbar.js
@@ -1,13 +1,36 @@
 function NavBar() {
     return (
         <>
-        <a href="#">BadBank</a>,
-        <a href="#/CreateAccount/">Create Account</a>,
-        <a href="#/login/">Login</a>,
-        <a href="#/deposit/">Deposit</a>,
-        <a href="#/withdraw/">Withdraw</a>,
-        <a href="#/balance/">Balance</a>,
-        <a href="#/alldata/">AllData</a>,
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">BadBank</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#/CreateAccount/">Create Account</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#/login/">Login</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#/deposit/">Deposit</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#/withdraw/">Withdraw</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#/balance/">Balance</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#/alldata/">AllData</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
         </>
     );
 }
