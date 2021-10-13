a4b3ef29010ec3830b0aa6e65073a09fc1ded6b9
Savannah Leasman <leasman7@gmail.com>
2021-10-13 14:18:40 -0500

    added deposit and withdraw cards (they do not change the balance yet)

--- deposit.js
+++ deposit.js
@@ -1,8 +1,56 @@
 function Deposit() {
-    const ctx = React.useContext(UserContext);
+    const [show, setShow] = React.useState(true);
+    const [status, setStatus] = React.useState("");
+    const [balance, setBalance] = React.useState('');
+    const [deposit, setDeposit] = React.useState('');
+    const ctx = React.useContext(UserContext); 
+  
+    function handleDeposit() {
+        
+        setShow(false);
+    }
+    
+    function clearForm() {
+        setShow(true);
+    }
+    
     return (
-        <h1>Deposit<br/>
-            {JSON.stringify(ctx)}
-        </h1>
-    );
+        <Card
+      bgcolor="success"
+      header="Deposit"
+      status={status}
+      body={
+        show ? (
+          <>
+            Balance
+            <br />
+            Deposit Amount
+            <input
+              type="input"
+              className="form-control"
+              id="deposit"
+              placeholder="Deposit Amount"
+              value={deposit}
+              onChange={(e) => setBalance(e.currentTarget.value)}
+            />
+            <br />
+            <button
+              type="submit"
+              className="btn btn-dark"
+              onClick={handleDeposit}
+            >
+              Deposit
+            </button>
+          </>
+        ) : (
+          <>
+            <h5>Success</h5>
+            <button type="submit" className="btn btn-dark" onClick={clearForm}>
+              Make another deposit
+            </button>
+          </>
+        )
+      }
+    />
+  );
 }
--- withdraw.js
+++ withdraw.js
@@ -1,8 +1,56 @@
 function Withdraw() {
-    const ctx = React.useContext(UserContext);
+    const [show, setShow] = React.useState(true);
+    const [status, setStatus] = React.useState("");
+    const [balance, setBalance] = React.useState('');
+    const [withdraw, setWithdraw] = React.useState('');
+    const ctx = React.useContext(UserContext); 
+  
+    function handleWithdraw() {
+        
+        setShow(false);
+    }
+    
+    function clearForm() {
+        setShow(true);
+    }
+    
     return (
-        <h1>Withdraw<br/>
-            {JSON.stringify(ctx)}
-        </h1>
-    );
+        <Card
+      bgcolor="danger"
+      header="Withdraw"
+      status={status}
+      body={
+        show ? (
+          <>
+            Balance
+            <br />
+            Withdraw Amount
+            <input
+              type="input"
+              className="form-control"
+              id="withdraw"
+              placeholder="Withdraw Amount"
+              value={withdraw}
+              onChange={(e) => setBalance(e.currentTarget.value)}
+            />
+            <br />
+            <button
+              type="submit"
+              className="btn btn-dark"
+              onClick={handleWithdraw}
+            >
+              Withdraw
+            </button>
+          </>
+        ) : (
+          <>
+            <h5>Success</h5>
+            <button type="submit" className="btn btn-dark" onClick={clearForm}>
+              Make another withdrawal
+            </button>
+          </>
+        )
+      }
+    />
+  );
 }
