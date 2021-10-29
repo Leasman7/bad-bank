117f5769a4cf143c9f82f67edb6e40f137b184e1
Savannah Leasman <leasman7@gmail.com>
2021-10-28 19:56:31 -0500

    updated home page message

--- home.js
+++ home.js
@@ -3,9 +3,9 @@ function Home() {
     <Card
       bgcolor="primary"
       txtcolor="white"
-      header="BadBank Landing Page"
+      header="BadBank"
       title="Welcome to the bank"
-      text="Create your account and get banking"
+      text="Where we serve all you non-secure banking needs"
       body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
     />
   );
