e41e56794f55e18c225f9f55323fb238bddf2b5d
Savannah Leasman <leasman7@gmail.com>
2022-01-10 16:53:50 -0600

    added module cards

--- modules.js
+++ modules.js
@@ -3,11 +3,11 @@ var ui = {};
 ui.navigation = `
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#" onclick="defaultModule()">BadBank</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link" href="#createAccount" onclick="loadCreateAccount()" id="createAccount">Create Account<a/>
         </li>
@@ -33,3 +33,90 @@ ui.navigation = `
 
 var navigation = document.getElementById('navigation');
 navigation.innerHTML += ui.navigation;
+
+ui.createAccount = `
+    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header">Create Account</div>
+        <div class="card-body">
+            Name<br>
+            <input type="input" class="form-control" id="name" placeholder="Enter name"><br>
+            Email address<br />
+            <input type="input" class="form-control" id="email" placeholder="Enter email"><br>
+            Password<br />
+            <input type="password" class="form-control" id="password" placeholder="Enter password"><br>
+            <button type="submit" id="submit" class="btn" onclick="create()">Create Account</button>
+            <div id='createStatus'></div>
+        </div>
+    </div>
+`;
+
+ui.login = `
+    <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+        <div class="card-holder">Login</div>
+        <div class="card-body">
+            Email<br>
+            <input type="input" class="form-control" id="loginEmail" placeholder="Enter Email"><br>
+            Password<br>
+            <input type="password" class="form-control" id="loginPassword" placeholder="Enter password"><br>
+            <button type="submit" id="submit" class="btn" onclick="login()">Login</button>
+            <div id="loginStatus"></div>
+        </div>
+    </div>
+`;
+
+ui.deposit = `
+    <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+        <div class="card-header">Deposit</div>
+        <div class="card-body">
+            Email<br>
+            <input type="input" class="form-control" id="depositEmail" placeholder="Enter email"><br>
+            Amount<br>
+            <input type="number" class="form-control" id="depositAmount" placeholder="Enter amount"><br>
+            <button type="submit" class="btn" onclick="deposit()">Deposit</button>
+            <div id="depositStatus"></div>
+        </div>
+    </div>
+`;
+
+ui.withdraw = `
+    <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
+        <div class="card-header">Withdraw</div>
+        <div class="card-body">
+            Email<br>
+            <input type="input" class="form-control" id="withdrawEmail" placeholder="Enter email"><br>
+            Amount<br>
+            <input type="number" class="form-control" id="withdrawAmount" placeholder="Enter amount"><br>
+            <button type="submit" class="btn" onclick="withdraw()">Withdraw</button>
+            <div id="withdrawStatus"></div>
+        </div>
+    </div>
+`;
+
+ui.balance = `
+    <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+        <div class="card-header">Balance</div>
+        <div class="card-body">
+            Email<br>
+            <input type="input" class="form-control" id="balanceEmail" placeholder="Enter email"><br>
+            <button type="submit" class="btn" onclick="balance()">Show Balance</button>
+            <div id="balanceStatus"></div>
+        </div>
+    </div>
+`;
+
+ui.default = `
+    <div class="card bg-dark mb-3" style="max-width: 18rem;">
+        <div class="card-header">BadBank Landing Module</div>
+        <div class="card-body">
+            <h5 class="card-title">Welcome to the bank</h5>
+            <p class="card-text">You can move around using the navigation bar.</p>
+            <img src="bank.png" class="img-fluid" alt="Responsive image">
+        </div>
+    </div>
+`;
+
+ui.allData = `
+    <h5>All Data in Store</h5>
+    <button type="button" class="btn btn-secondary" onclick="allData()">Show All Data</button>
+    <div id="allDataStatus"></div>
+`;
