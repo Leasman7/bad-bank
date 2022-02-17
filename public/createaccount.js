7bcb745bec5524ba1371194aa5f246c62265eb50
Savannah Leasman <leasman7@gmail.com>
2022-02-17 10:31:26 -0600

    fetch updated

--- public/createaccount.js
+++ public/createaccount.js
@@ -46,8 +46,7 @@ function CreateAccount() {
     console.log(requestOptions)
     fetch('http://' + window.location.hostname + ':3000/account/create', requestOptions)
         .then(response => response.json())
-        .then(data => console.log(data) //this.setState({ name: data.name })
-        );
+        .then(data => console.log(data));
   }
 
   function clearForm() {
